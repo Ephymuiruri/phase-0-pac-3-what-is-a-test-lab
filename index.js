a4a8ad11f3d6afc65f9742556b45c5c83ee337e4
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-03-14 10:09:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height= 10
+if(height >0 && height <40) {console.log("height is within the range")} 
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
